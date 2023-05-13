9ca394729ddccb980aacba98002389796b1c231c
hyeok3011 <hyeok3011@gmail.com>
2023-05-13 21:20:40 +0900

    resolve 9.palindrome_number/golang Runtime 12ms Beats 94.46%  Memory 4.3MB Beats 99.96%

---
+++ problems/9.palindrome_number/golang/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func Test_isPalindrome(t *testing.T) {
+	assert.Equal(t, isPalindrome(121), true)
+	assert.Equal(t, isPalindrome(-121), false)
+	assert.Equal(t, isPalindrome(10), false)
+}
