841ff37efd3685a2beca863310f116a1399c5383
hyeok3011 <hyeok3011@gmail.com>
2023-06-16 12:32:23 +0900

    solve 13.roman_to_integer Runtime 54 ms Beats 83.83% Memory 16.3 MB Beats 36.27%

---
+++ problems/13.roman_to_integer/python3/solution.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/roman-to-integer/description/
+# Runtime 54 ms Beats 83.83% Memory 16.3 MB Beats 36.27%
+ROMAN = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000             
+        }
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        i = len(s) - 1
+        integer = 0
+        last_num = 0
+        while i >= 0:
+            current = ROMAN[s[i]]
+            if last_num <= current:
+                integer += current
+            else:
+                integer -= current
+            last_num = current
+            i -= 1
+        return integer
+    
