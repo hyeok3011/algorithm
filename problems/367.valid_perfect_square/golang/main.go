b3fc7bac329be1319fd425c2482ebcfb8c0cbf7b
hyeok3011 <hyeok3011@gmail.com>
2023-06-26 17:06:55 +0900

    add a solution for 367 with binary search (golang) version

--- problems/367.valid_perfect_square/golang/main.go
+++ problems/367.valid_perfect_square/golang/main.go
@@ -17,3 +17,26 @@ func isPerfectSquare(num int) bool {
 	}
 	return false
 }
+
+// Binary search version
+func isPerfectSquare2(num int) bool {
+	if num == 1 {
+		return true
+	}
+	left, right := 2, num/2
+
+	for left <= right {
+		mid := left + (right-left)/2
+		midSquared := mid * mid
+
+		if midSquared == num {
+			return true
+		} else if midSquared < num {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return false
+}
