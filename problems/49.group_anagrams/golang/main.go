9424b63552749abb8a4dfe740e34c4478b669782
hyeok3011 <hyeok3011@gmail.com>
2023-06-30 22:43:39 +0900

    solve 49.group_anagrams Runtime 24 ms Beats 79.10% Memory 7.3 MB Beats 91.32%

---
+++ problems/49.group_anagrams/golang/main.go
@@ -0,0 +1,31 @@
+package main
+
+// https://leetcode.com/problems/group-anagrams/description/
+// Runtime 24 ms Beats 79.10% Memory 7.3 MB Beats 91.32%
+func groupAnagrams(words []string) [][]string {
+	group := make(map[string][]string)
+
+	for _, word := range words {
+		runeLetters := make([]rune, 26)
+		for _, letter := range word {
+			if runeLetters[letter-97] == 0 {
+				runeLetters[letter-97] = letter
+			} else {
+				runeLetters[letter-97] += 1
+			}
+		}
+
+		if _, ok := group[string(runeLetters)]; ok {
+			group[string(runeLetters)] = append(group[string(runeLetters)], word)
+		} else {
+			group[string(runeLetters)] = []string{word}
+		}
+	}
+
+	result := [][]string{}
+	for _, val := range group {
+		result = append(result, val)
+	}
+
+	return result
+}
