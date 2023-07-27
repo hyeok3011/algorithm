ef3449724995481c2c770a54d2626212afc3f967
hyeok3011 <hyeok3011@gmail.com>
2023-07-27 12:24:15 +0900

    solve 182.duplicate_emails Runtime 740 ms

---
+++ leetcode/problems/182.duplicate_emails/solution.sql
@@ -0,0 +1,15 @@
+-- Runtine Runtime 858 ms
+SELECT email AS Email FROM Person GROUP BY email HAVING COUNT(*) > 1;
+
+
+-- Runtime 740 ms
+SELECT p1.email AS Email
+FROM Person p1
+INNER JOIN Person p2 ON p1.email = p2.email AND p1.id <> p2.id
+group by Email
+
+-- 다른사람의 솔류션 단순 중복제거를 위함이면 group by보다 DISTINCT 더 효율적일듯 하다
+-- Runtime 700 ms
+SELECT DISTINCT(p1.email) from 
+Person p1 JOIN Person p2 ON
+p1.email = p2.email AND p1.id <> p2.id;
