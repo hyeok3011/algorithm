69e1879cf1c9e5a53561ed9d2a603becd0d4a60c
hyeok3011 <hyeok3011@gmail.com>
2024-04-30 16:37:01 +0900

    solve 670.maximum_swap

---
+++ leetcode/problems/670.maximum_swap/solution.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-swap/description/
+// 
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function(num) {
+    let numSplite = num.toString().split("");
+    let maxIndex = -1, maxVal = -1, leftIndex = -1, swapIndex = -1;
+
+    for (let i = numSplite.length - 1; i >= 0; i--) {
+        let digit = parseInt(numSplite[i]);
+
+        if (digit > maxVal) {
+            maxVal = digit;
+            maxIndex = i;
+        } else if (digit < maxVal) {
+            leftIndex = i;
+            swapIndex = maxIndex;
+        }
+    }
+
+    [numSplite[leftIndex], numSplite[swapIndex]] = [numSplite[swapIndex],numSplite[leftIndex]];
+    return parseInt(numSplite.join(""));
+};
