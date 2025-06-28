f50d3f8fe3652854c83a72aee62b228604d203ef
hyeok3011 <hyeok3011@gmail.com>
2025-06-28 20:48:49 +0900

    feat(programmers/lv2/비밀코드해독/python)

---
+++ programmers/lv2/비밀코드해독/python/solution.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/388352?language=python3
+
+import itertools
+
+def solution(n, q, anw):
+    valid_code_count = 0
+    
+    for candidate in itertools.combinations(range(1, n + 1), 5):
+        candidate_set = set(candidate)
+        
+        match_count = all(
+            # python에서 set연산이 이렇게 가능하다니...
+            len(candidate_set & set(input_data)) == answer
+            for input_data, answer in zip(q, anw)
+        )
+        
+        if match_count:
+            valid_code_count += 1
+            
+    return valid_code_count
