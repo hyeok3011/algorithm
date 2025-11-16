7eb9e7700460e2b79420b8c9b20cad41a84a7d30
강재혁 (joel) <hyeok3011@gmail.com>
2025-11-16 14:59:54 +0900

    feat(leetcode/problems/easy/680.valid-palindrome-ii/go)

---
+++ leetcode/problems/easy/680.valid-palindrome-ii/go/main.go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-palindrome-ii/
+func validPalindrome(s string) bool {
+    left := 0
+    right := len(s) - 1
+    for left <= right {
+        if s[left] != s[right] {
+            return isPalindrome(s, left+1, right) || isPalindrom(s, left, right - 1)
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+func isPalindrome(s string, left, right int) bool {
+    for left <= right {
+        if s[left] != s[right] {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
