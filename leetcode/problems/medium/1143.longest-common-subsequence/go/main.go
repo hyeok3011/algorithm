e5ceadd9212ec729d742b9da8f25bc21294b60f1
강재혁 (joel) <hyeok3011@gmail.com>
2025-12-22 22:18:24 +0900

    feat(leetcode/problems/medium/1143.longest-common-subsequence/go)

---
+++ leetcode/problems/medium/1143.longest-common-subsequence/go/main.go
@@ -0,0 +1,20 @@
+package main
+
+// https://leetcode.com/problems/longest-common-subsequence/?envType=study-plan-v2&envId=leetcode-75
+func longestCommonSubsequence(text1 string, text2 string) int {
+    dp := make([][]int, len(text1) + 1)
+    for i := 0; i < len(dp); i++ {
+        dp[i] = make([]int, len(text2) + 1)
+    }
+
+    for i := 0; i < len(text1); i++ {
+        for j := 0; j < len(text2); j++ {
+            if text1[i] == text2[j] {
+                dp[i + 1][j + 1] = dp[i][j] + 1
+            } else {
+                dp[i + 1][j + 1] = max(dp[i+1][j], dp[i][j+1])
+            }
+        }
+    }
+    return dp[len(text1)][len(text2)]
+}
