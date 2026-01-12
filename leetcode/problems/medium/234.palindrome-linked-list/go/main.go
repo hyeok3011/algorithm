1c13a45cc6a9f18b355c9916969f72b305a7d987
강재혁 (joel) <hyeok3011@gmail.com>
2026-01-12 22:08:32 +0900

    feat(leetcode/problems/medium/234.palindrome-linked-list/go)

---
+++ leetcode/problems/medium/234.palindrome-linked-list/go/main.go
@@ -0,0 +1,36 @@
+package main
+
+// https://leetcode.com/problems/palindrome-linked-list/
+func isPalindrome(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return true
+    }
+
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	prev := slow
+	current := slow
+	next := slow.Next
+
+	for next != nil {
+		temp := next.Next
+		current.Next = prev
+		next.Next = current
+		prev = current
+		current = next
+		next = temp
+	}
+
+	for current != head {
+		if head.Val != current.Val {
+			return false
+		}
+		head = head.Next
+		current = current.Next
+	}
+	return true
+}
